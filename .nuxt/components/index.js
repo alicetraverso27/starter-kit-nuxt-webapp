e9204067eaceb865f493e3511a7fb2c2cc305c4a
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-02-12 00:23:42 +0100

    Update

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MyComponent } from '../../components/MyComponent.vue'
-
-export const LazyMyComponent = import('../../components/MyComponent.vue' /* webpackChunkName: "components/my-component" */).then(c => c.default || c)
