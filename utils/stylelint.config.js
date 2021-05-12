0d37aa79e035241225733a130cb64f2a58841cee
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-05-12 17:08:04 +0200

    Update

--- utils/stylelint.config.js
+++ utils/stylelint.config.js
@@ -7,9 +7,5 @@ module.exports = {
     // recommended rules
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
-
-    // any other rules you'd want to change e.g.
-    'scss/dollar-variable-pattern': '^foo',
-    'scss/selector-no-redundant-nesting-selector': true,
   },
 }
