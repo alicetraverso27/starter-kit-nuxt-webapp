b77f586b2a55953570e51bdf778bea21b67e0a30
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-07-23 16:13:37 +0200

    Update

--- app/configuration/styleResources.js
+++ app/configuration/styleResources.js
@@ -1,6 +1,9 @@
 // https://github.com/nuxt-community/style-resources-module
 export default {
   styleResources: {
-    scss: ['utils/scss/vars/_index.scss', 'utils/scss/mixins/_index.scss'],
+    scss: [
+      '~/assets/scss/vars/_index.scss',
+      '~/assets/scss/mixins/_index.scss',
+    ],
   },
 }
