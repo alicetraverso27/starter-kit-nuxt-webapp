d9de60a276659cd22d224d64261b8ef8a2a6e1b3
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-09-10 12:48:32 +0200

    DCDCDCDCDC

--- app/configuration/build.js
+++ app/configuration/build.js
@@ -1,15 +1,9 @@
 // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-build
-import { resolve } from 'path'
 
 export default {
   build: {
     transpile: ['gsap'],
-    extend(config, ctx) {
-      // Add '~/library' as an alias.
-      // https://github.com/nuxt/vercel-builder/blob/main/examples/side-by-side/README.md
-      config.resolve.alias.shared = resolve(__dirname, '../../library')
-      config.resolve.alias['~library'] = resolve(__dirname, '../../library')
-    },
+    extend(config, ctx) {},
     cssSourceMap: false,
   },
 }
