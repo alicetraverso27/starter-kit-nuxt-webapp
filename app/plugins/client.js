2be948140cddf927d4e63eed6e60dc5383360331
Davide <io@davidemarchet.com>
2021-09-17 22:23:14 +0200

    retest

--- app/plugins/client.js
+++ app/plugins/client.js
@@ -1,4 +1,4 @@
-/* import Vue from 'vue'
+import Vue from 'vue'
 import locomotiveScroll from '~library/plugins/locomotive-scroll'
 import viewportSizes from '~library/plugins/viewport-sizes'
 import detectHoverTouch from '~library/plugins/detect-hover-touch'
@@ -7,4 +7,4 @@ import pointerMove from '~library/plugins/pointer-move'
 Vue.use(locomotiveScroll)
 Vue.use(viewportSizes)
 Vue.use(detectHoverTouch)
-Vue.use(pointerMove) */
+Vue.use(pointerMove)
