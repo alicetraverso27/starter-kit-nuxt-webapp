03c61e133c232d69202e4b0d1e9eb68adc0fd956
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-10-12 17:29:31 +0200

    udpate

--- app/configuration/build.js
+++ app/configuration/build.js
@@ -7,7 +7,7 @@ export default {
     extend(config, ctx) {
       // Add '~/library' as an alias.
       // https://github.com/nuxt/vercel-builder/blob/main/examples/side-by-side/README.md
-      config.resolve.alias.shared = resolve(__dirname, '../../library')
+      config.resolve.alias.library = resolve(__dirname, '../../library')
       config.resolve.alias['~library'] = resolve(__dirname, '../../library')
     },
     cssSourceMap: false,
