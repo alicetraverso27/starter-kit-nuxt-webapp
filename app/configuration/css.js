e1d71127fe1d350d00fb59bf73611b1c90f73d37
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-10-12 18:16:04 +0200

    Update

--- app/configuration/css.js
+++ app/configuration/css.js
@@ -1,5 +1,5 @@
 // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
 export default {
-  css: ['~/styles/scss/layout/_index.scss', '~/styles/scss/typo/_index.scss'],
+  css: ['~/assets/scss/layout/_index.scss', '~/assets/scss/typo/_index.scss'],
   // css: ['~/assets/css/main.css', '~/assets/css/animations.scss']
 }
